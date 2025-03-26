2399d5bb2f5b13d60292d4ca912350f0244879b2
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2025-03-26 01:37:54 -0400

    UpdatedMainActivity

--- app/src/main/java/com/example/roverphotos/MainActivity.kt
+++ app/src/main/java/com/example/roverphotos/MainActivity.kt
@@ -1,45 +1,25 @@
 package com.example.roverphotos
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.roverphotos.ui.screens.RoverViewModel
-import com.example.roverphotos.ui.theme.ui.screens.HomeScreen
-import com.example.roverphotos.ui.theme.ui.screens.RoverDetailScreen
-import com.example.roverphotos.ui.theme.ui.theme.RoverPhotosTheme
+import androidx.compose.runtime.Composable
+import androidx.core.app.ComponentActivity
+import com.example.roverphotos.ui.theme.MarsPhotosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RoverPhotosTheme {
-                val navController = rememberNavController()
-
-                NavHost(navController = navController, startDestination = "home") {
-                    composable("home") {
-                        val viewModel: RoverViewModel = viewModel()
-                        HomeScreen(
-                            viewModel = viewModel,
-                            onRoverClick = { rover ->
-                                navController.navigate("detail/${rover.name}/2025-03-25")
-                            }
-                            )
-                            onRoverClick = { rover ->
-                                navController.navigate("detail/${rover.name}/2025-03-25")
-                            }
-                    }
-
-                    composable("detail/{roverName}/{earthDate}") { backStackEntry ->
-                        val roverName = backStackEntry.arguments?.getString("roverName") ?: ""
-                        val earthDate = backStackEntry.arguments?.getString("earthDate") ?: ""
-                        RoverDetailScreen(navController, roverName, earthDate)
-                    }
-                }
+            MyApp {
+                Navigation()
             }
         }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MarsPhotosTheme {
+        content()
+        
     }
 }
