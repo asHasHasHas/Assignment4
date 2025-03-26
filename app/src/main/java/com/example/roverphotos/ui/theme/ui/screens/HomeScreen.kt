401a3e672b299ce2d0c5ad44b0576a900f299452
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2025-03-25 23:55:34 -0400

    DeletedUselessImports

--- app/src/main/java/com/example/roverphotos/ui/theme/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/roverphotos/ui/theme/ui/screens/HomeScreen.kt
@@ -2,25 +2,20 @@ package com.example.roverphotos.ui.theme.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.example.roverphotos.R
 import com.example.roverphotos.model.Rover
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
-import androidx.compose.ui.draw.scale
 
 @Composable
 fun HomeScreen(
@@ -30,7 +25,7 @@ fun HomeScreen(
 ){
     LazyColumn(modifier = modifier) {
         items(roverList) { rover ->
-            RoverCard(modifier = Modifier.scale(0.8f), rover=rover, onClick = { onRoverClick(rover) })
+            RoverCard(modifier = Modifier.width(350.dp), rover=rover, onClick = { onRoverClick(rover) })
         }
     }
 }
