4a3212891e58a062eb206be6590bbb42b6630c2a
asHasHasHas <168456398+asHasHasHas@users.noreply.github.com>
2025-03-26 22:33:06 -0400

    rover update

--- app/src/main/java/com/example/roverphotos/model/Rover.kt
+++ app/src/main/java/com/example/roverphotos/model/Rover.kt
@@ -4,5 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class Rover(
     @SerializedName("name") val name: String,
-    @SerializedName("launch_date") val launchDate: String
+    @SerializedName("launch_date") val launchDate: String,
+    @SerializedName("img_src") val imgSrc: String
 )
